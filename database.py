2052f10a5763a6c0d15f9d964002a978a721351b
Akeem Henry <henryakeem72@gmail.com>
2022-12-29 12:13:06 -0500

    Inital Commit

---
+++ database.py
@@ -0,0 +1,19 @@
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import create_engine
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///blog.sqlite3"
+
+DB_USER: str = "postgres"
+DB_NAME: str = "Fastapi"
+DB_PASS: str = "MADman123"
+PORT: int = 5432
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@localhost:{PORT}/{DB_NAME}"
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
